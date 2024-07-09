3b5133a7dda992389e82605e776a0dfe8afcca6a
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2024-07-10 00:07:32 +0530

    Created the application

---
+++ StudySprout/app/Pages/Login.js
@@ -0,0 +1,11 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+
+export default function ogin() {
+  return (
+    <View>
+        <Text>StudySprout</Text>
+          <Image source={require('/Users/harshvardhanupadhyay/STUDYSPROUT/StudySprout/StudySprout/assets/images/Group 6.png')}></Image>
+    </View>
+  )
+}
---
+++ StudySprout/app/index.js
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Login from './Pages/Login.js'
+
+export default function Index() {
+  return (
+    <View>
+<Login/>
+
+    </View>
+  );
+}
+
